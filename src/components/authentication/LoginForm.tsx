916c5655a2f16ae16d911dbdbeac1ccae0eb3197
Jayadithya <jganji@adobe.com>
2024-12-25 16:36:00 +0530

    added sidebar and page skeletons

--- src/components/authentication/LoginForm.tsx
+++ src/components/authentication/LoginForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useId, useState } from 'react'
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -13,7 +13,11 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { toast } from "sonner"
 import { cn } from '@/lib/utils'
+import { login } from '@/app/actions/auth-actions'
+import { redirect } from 'next/navigation'
+import { Loader2 } from 'lucide-react'
 
 const formSchema = z.object({
     email: z.string().email({
@@ -25,6 +29,8 @@ const formSchema = z.object({
 })
 
 const LoginForm = () => {
+    const [loading, setLoading] = useState(false);
+    const toastId = useId();
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
@@ -34,9 +40,26 @@ const LoginForm = () => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        toast.loading('Logging in...', { id: toastId })
+        setLoading(true)
+
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
+        const formData = new FormData()
+        formData.append('email', values.email)
+        formData.append('password', values.password)
+
+        const { success, error } = await login(formData)
+        if (!success) {
+            toast.error("A problem occured while logging in! Check your email and password", { id: toastId })
+            setLoading(false)
+        } else {
+            toast.success("Logged in successfully", { id: toastId })
+            setLoading(false)
+            redirect('/dashboard')
+        }
+
         console.log(values)
     }
 
@@ -71,7 +94,10 @@ const LoginForm = () => {
                             </FormItem>
                         )}
                     />
-                    <Button className='w-full' type="submit">Login</Button>
+                    <Button disabled={loading} className='w-full' type="submit">
+                        {loading && <Loader2 className='mr-2 h-4 w-4 animate-spin' />}
+                        Login
+                        </Button>
 
                 </form>
             </Form>
